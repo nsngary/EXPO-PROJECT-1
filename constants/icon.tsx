bd13bf2d168ebac1fd247363e8978f391f05f8e3
重光 <gary@wellmarket.com.tw>
2025-12-29 16:26:28 +0800

    fix multiple-import in icon.tsx

--- constants/icon.tsx
+++ constants/icon.tsx
@@ -1,10 +1,9 @@
-import { Feather } from "@expo/vector-icons";
-import { FontAwesome } from "@expo/vector-icons";
+import { Feather, FontAwesome } from "@expo/vector-icons";
 
-    export const icon = {
-        index: (props:any) => <Feather name='home' size={24} {...props} />,
-        performance: (props:any) => <Feather name='bar-chart-2' size={24} {...props} />,
-        order: (props:any) => <FontAwesome name='shopping-bag' size={24} {...props} />,
-        event: (props:any) => <Feather name='calendar' size={24} {...props} />,
-        profile: (props:any) => <Feather name='user' size={24} {...props} />,
-    }
+export const icon = {
+    index: (props:any) => <Feather name='home' size={24} {...props} />,
+    performance: (props:any) => <Feather name='bar-chart-2' size={24} {...props} />,
+    order: (props:any) => <FontAwesome name='shopping-bag' size={24} {...props} />,
+    event: (props:any) => <Feather name='calendar' size={24} {...props} />,
+    profile: (props:any) => <Feather name='user' size={24} {...props} />,
+}
