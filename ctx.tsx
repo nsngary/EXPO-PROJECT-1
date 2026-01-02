34ebfc244f88a7621cd8437309665d37f90ba835
重光 <gary@wellmarket.com.tw>
2026-01-02 14:19:53 +0800

    fix npx expo-doctor

---
+++ ctx.tsx
@@ -0,0 +1,43 @@
+import { use, createContext, type PropsWithChildren } from 'react';
+import { useStorageState } from './useStorageState';
+
+const AuthContext = createContext<{
+  signIn: () => void;
+  signOut: () => void;
+  session?: string | null;
+  isLoading: boolean;
+}>({
+  signIn: () => null,
+  signOut: () => null,
+  session: null,
+  isLoading: false,
+});
+
+// Use this hook to access the user info.
+export function useSession() {
+  const value = use(AuthContext);
+  if (!value) {
+    throw new Error('useSession must be wrapped in a <SessionProvider />');
+  }
+  return value;
+}
+
+export function SessionProvider({ children }: PropsWithChildren) {
+  const [[isLoading, session], setSession] = useStorageState('session');
+  return (
+    <AuthContext.Provider
+      value={{
+        signIn: () => {
+          // Perform sign-in logic here
+          setSession('xxx');
+        },
+        signOut: () => {
+          setSession(null);
+        },
+        session,
+        isLoading,
+      }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
